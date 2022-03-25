17308b2d7e7c05bcfd9cabb4eaf32f24235808ea
Kingstalux <kingsleyngu25@gmail.com>
2022-03-25 14:42:11 +0100

    Display components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import Signin from './components/SignIn';
+import SignUp from './components/SignUp';
+import Items from './components/Items';
+import Details from './components/Details';
+import Reservation from './components/Reservation';
+import MyReservations from './components/MyReservations';
+import DeleteItem from './components/DeleteItem';
+import AddItem from './components/AddItem';
 
 function App() {
   return (
     <div className="App">
-
-      Components
+      <NavBar />
+      <Signin />
+      <SignUp />
+      <Items />
+      <Details />
+      <Reservation />
+      <MyReservations />
+      <DeleteItem />
+      <AddItem />
     </div>
   );
 }
