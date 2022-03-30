e0668a851bb8adf879f3bc417198fe4ed604efe3
Somdotta07 <jhilick.s@gmail.com>
2022-03-30 19:29:34 +0200

    Add vertical navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
+// import NavBar from './components/NavBar';
 // import Signin from './components/SignIn';
 // import SignUp from './components/SignUp';
 import Item from './components/Item';
 import DetailsPage from './components/Details/DetailsPage';
 import ReservationForm from './components/Reservation/ReservationForm';
 import Reservation from './components/Reservation/Reservation';
+import VerticalNav from './components/VerticalNav';
 // import DeleteItem from './components/DeleteItem';
 // import AddItem from './components/AddItem';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Routes>
+      {/* <NavBar /> */}
+      <div className="col-2 p-0">
+        <VerticalNav />
+      </div>
+      <div className="col-md-10 col-sm-12 ">
+        <Routes>
 
-        <Route exact path="/" element={<Item />} />
-        <Route path="/Details/:id" element={<DetailsPage />} />
-        <Route path="/reserve" element={<ReservationForm />} />
-        <Route path="/reservations" element={<Reservation />} />
-      </Routes>
-
+          <Route exact path="/" element={<Item />} />
+          <Route path="/Details/:id" element={<DetailsPage />} />
+          <Route path="/reserve" element={<ReservationForm />} />
+          <Route path="/reservations" element={<Reservation />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/VerticalNav.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { FaFacebookF, FaGooglePlus } from 'react-icons/fa';
+import { GrPinterest } from 'react-icons/gr';
+import { BsTwitter, BsVimeo } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+
+const VerticalNav = () => {
+  const navigate = useNavigate();
+  const [clickedIndex, setClickedIndex] = useState(0);
+  return (
+    <aside className="p-3 d-sm-block d-none">
+      <h1>Arenas</h1>
+      <div className="sidebar-items">
+        <button type="button" onClick={() => { setClickedIndex(0); navigate('/'); }} className="bg-transparent borderless w-100 text-start">
+          <h5 className={`${clickedIndex === 0 && 'sidebar-item-active '} py-2 fw-bold ps-2`}>ARENAS</h5>
+        </button>
+        <button type="button" onClick={() => { setClickedIndex(1); navigate('/reserve'); }} className="bg-transparent borderless w-100 text-start">
+          <h5 className={`${clickedIndex === 1 && 'sidebar-item-active '} py-2 fw-bold ps-2`}>RESERVE</h5>
+        </button>
+        <button type="button" onClick={() => { setClickedIndex(2); navigate('/reservations'); }} className="bg-transparent borderless w-100 text-start">
+          <h5 className={`${clickedIndex === 2 && 'sidebar-item-active '} py-2 fw-bold ps-2`}>MY RESERVATIONS </h5>
+        </button>
+        <button type="button" onClick={() => { setClickedIndex(3); navigate('/AddItem'); }} className="bg-transparent borderless w-100 text-start">
+          <h5 className={`${clickedIndex === 3 && 'sidebar-item-active '} py-2 fw-bold ps-2`}>ADD ARENAS</h5>
+        </button>
+        <button type="button" onClick={() => { setClickedIndex(4); navigate('/DeleteItem'); }} className="bg-transparent borderless w-100 text-start">
+          <h5 className={`${clickedIndex === 4 && 'sidebar-item-active '} py-2 fw-bold ps-2`}>DELETE </h5>
+        </button>
+      </div>
+      <div className="sidebar-footer">
+        <div className="sidebar-icons">
+          <span className="p-2">
+            <BsTwitter />
+          </span>
+          <span className="p-2">
+            <FaFacebookF />
+          </span>
+          <span className="p-2">
+            <FaGooglePlus />
+          </span>
+          <span className="p-2">
+            <BsVimeo />
+
+          </span>
+          <span className="p-2">
+            <GrPinterest />
+          </span>
+        </div>
+        <div className="text-wrap">© 2022 Final capstone project</div>
+      </div>
+      <div />
+    </aside>
+  );
+};
+
+export default VerticalNav;
