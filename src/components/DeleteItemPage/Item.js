b856c09a4e1ec44c61a30ce67622ec42a896e2e0
od-c0d3r <omar_rashad@aol.com>
2022-03-31 07:43:34 +0200

    Create: item props type validation

--- src/components/DeleteItemPage/Item.js
+++ src/components/DeleteItemPage/Item.js
@@ -1,25 +1,24 @@
+import PropTypes from 'prop-types';
 import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 
-const EventCard = () => (
-  <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-    <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card&apos;s content.
-      </Card.Text>
-    </Card.Body>
-    <ListGroup className="list-group-flush">
-      <ListGroupItem>Cras justo odio</ListGroupItem>
-      <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-      <ListGroupItem>Vestibulum at eros</ListGroupItem>
-    </ListGroup>
-    <Card.Body>
-      <Card.Link href="#">Card Link</Card.Link>
-      <Card.Link href="#">Another Link</Card.Link>
-    </Card.Body>
-  </Card>
-);
+const EventCard = (props) => {
+  const { item } = props;
+
+  return (
+
+  );
+};
+
+EventCard.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    city: PropTypes.string,
+    price: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    capacity: PropTypes.string,
+  }).isRequired,
+};
 
 export default EventCard;
