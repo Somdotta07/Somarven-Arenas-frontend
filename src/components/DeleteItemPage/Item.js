92273ea778dc3c16a99fd2cff5a2872a7c5f2404
od-c0d3r <omar_rashad@aol.com>
2022-03-31 08:19:21 +0200

    Delete: item action in item component

--- src/components/DeleteItemPage/Item.js
+++ src/components/DeleteItemPage/Item.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import {
   Button,
   Card,
@@ -6,9 +7,16 @@ import {
   ListGroup,
   ListGroupItem,
 } from 'react-bootstrap';
+import getReserved from '../../api/reserved';
 
 const EventCard = (props) => {
   const { item } = props;
+  const dispatch = useDispatch();
+
+  const deleteItem = (id) => {
+    dispatch(deleteReserve(id));
+    dispatch(getReserved());
+  };
 
   return (
     <Col className="d-flex justify-content-center ">
@@ -26,7 +34,7 @@ const EventCard = (props) => {
           <ListGroupItem>{`$${item.price} / Day`}</ListGroupItem>
         </ListGroup>
         <Card.Body className="d-flex justify-content-center align-items-end">
-          <Button variant="danger">Delete</Button>
+          <Button variant="danger" onChange={deleteItem(item.id)}>Delete</Button>
         </Card.Body>
       </Card>
     </Col>
@@ -38,7 +46,7 @@ EventCard.propTypes = {
     id: PropTypes.number,
     name: PropTypes.string,
     city: PropTypes.string,
-    price: PropTypes.string,
+    price: PropTypes.number,
     image: PropTypes.string,
     description: PropTypes.string,
     capacity: PropTypes.string,
